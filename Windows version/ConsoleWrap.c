b4b2bde45b99a2b792eac0221b353b7b07b68901
Fene4ka <old_g_case@mail.ru>
2016-05-27 15:13:46 +0600

    Update ConsoleWrap.c

--- Windows version/ConsoleWrap.c
+++ Windows version/ConsoleWrap.c
@@ -1,4 +1,3 @@
-
 #ifdef _MSC_VER
 #define _CRT_SECURE_NO_WARNINGS
 #endif
@@ -81,25 +80,6 @@ BOOL InitConsole(LPSTR Title, SHORT x, SHORT y, WORD color)
 	return result;
 }
 
-
-
-// Gets current console size
-/*inline*/ COORD GetSize()
-{
-	return console.propEx.dwSize;
-}
-
-
-
-
-// Gets current cursor position in console
-/*inline*/ COORD GetPos()
-{
-	return console.pos;
-}
-
-
-
 // Sets the cursor position to specified coordinates
 /*inline*/ BOOL SetPos(SHORT x, SHORT y)
 {
@@ -116,78 +96,7 @@ BOOL InitConsole(LPSTR Title, SHORT x, SHORT y, WORD color)
 	return SetConsoleTextAttribute(console.hOut, color);
 }
 
-// Sets the color attributes for console output
-/*inline*/ BOOL FlushConsole(WORD color)
-{
-	DWORD count = console.propEx.dwSize.X*console.propEx.dwSize.Y;
-	console.pos.X = 0;
-	console.pos.Y = 0;
-	return FillConsoleOutputAttribute(console.hOut, color, count, console.pos, &tmpDword) && FillConsoleOutputCharacter(console.hOut, ' ', count, console.pos, &tmpDword);
-}
 
-
-// Changes console window size to specified rows and columns count
-BOOL ResizeConsole(SHORT x, SHORT y)
-{
-	BOOL result;
-
-	// Screen buffer info obtaining and setup
-	console.propEx.cbSize = sizeof(console.propEx);  // (!) structure size needs to be specified first
-	result = GetConsoleScreenBufferInfoEx(console.hOut, &console.propEx);
-	console.propEx.dwSize.X = console.propEx.srWindow.Right = console.propEx.dwMaximumWindowSize.X = x;
-	console.propEx.dwSize.Y = console.propEx.srWindow.Bottom = console.propEx.dwMaximumWindowSize.Y = y;
-	// console.propEx.bFullscreenSupported = TRUE; /* Makes no sense */
-	result = result && SetConsoleScreenBufferInfoEx(console.hOut, &console.propEx);
-	return result;
-}
-
-// Changes console window size to specified rows and columns count, filling new areas with specified color
-/*inline*/ BOOL ResizeConsoleEx(SHORT x, SHORT y, WORD color)
-{
-	console.pos.X = 0;
-	console.pos.Y = 0;
-	return SetConsoleTextAttribute(console.hOut, color) && ResizeConsole(x, y) &&
-		FillConsoleOutputAttribute(console.hOut, color, x*y, console.pos, &tmpDword);
-}
-
-
-
-// Retrieves singlebyte character from current cursor position
-/*inline*/ CHAR GetCurrentChar()
-{
-	CHAR result = 0x0;
-	ReadConsoleOutputCharacter(console.hOut, &result, 1, console.pos, &tmpDword);
-	return result;
-}
-
-// Retrieves singlebyte character from current cursor position
-/*inline*/ CHAR GetChar(SHORT x, SHORT y)
-{
-	CHAR result = 0x0;
-	console.pos.X = x;
-	console.pos.Y = y;
-	ReadConsoleOutputCharacter(console.hOut, &result, 1, console.pos, &tmpDword);
-	return result;
-}
-
-// Retrieves singlebyte character from current cursor position
-/*inline*/ CHAR GetCharEx(COORD _pos)
-{
-	CHAR result = 0x0;
-	ReadConsoleOutputCharacter(console.hOut, &result, 1, _pos, &tmpDword);
-	return result;
-}
-
-// Writes singlebyte character to console output with selected attributes from cursor position
-/*inline*/ BOOL SetCurrentChar(WORD color, CHAR ch)
-{
-	if (SetConsoleTextAttribute(console.hOut, color))
-	{
-		putchar(ch);
-		return TRUE;
-	}
-	return FALSE;
-}
 
 // Writes singlebyte character to console output with selected attributes from cursor position
 /*inline*/ BOOL SetChar(SHORT x, SHORT y, WORD color, CHAR ch)
@@ -233,11 +142,6 @@ extern /*inline*/ BOOL Write(WORD color, LPSTR string)
 	}
 	return FALSE;
 }
-
-
-
-
-
 
 
 // Internal error info text buffer
