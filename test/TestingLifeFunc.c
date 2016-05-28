b43011f0c6ebb6af3bf7b5dbebdf31e5e1269fdb
TufTeddy <godisunstoppable@gmail.com>
2016-05-28 11:19:52 +0700

    Update TestingLifeFunc.c

--- test/TestingLifeFunc.c
+++ test/TestingLifeFunc.c
@@ -1,12 +1,12 @@
 #include <stdlib.h>
 #include "ctest.h"
-#include "FUNC.h"
+#include "main.cpp"
 
 CTEST(testLife, TestClear) {
 	int A[3][3] = { {1, 1, 1}, {1, 1, 1}, {1, 1, 1} };
 	int B[3][3] = { {3, 3, 3}, {3, 3, 3}, {3, 3, 3} };
 	
-	Clear(A, 3, 3);
+	clear(A, 3, 3);
 	
 	for(i = 0; i < 4; i++){
 		for(j = 0; j < 4; j++){
@@ -19,7 +19,7 @@ CTEST(testLife, TestClearScope) {
 	int A[3][3] = { { 1, 1, 1 },{ 1, 1, 1 },{ 1, 1, 1 } };
 	int B[3][3] = { { 3, 3, 3 },{ 3, 1, 3 },{ 3, 3, 3 } };
 	
-	ClearScope(A, 3, 3);
+	clear_Scope(A, 3, 3);
 	
 	for(i = 0; i < 4; i++){
 		for(j = 0; j < 4; j++){
@@ -29,23 +29,11 @@ CTEST(testLife, TestClearScope) {
 
 }
 
-CTEST(testLife, TestCh) {
-	int A = Ch(A, 3, 3);;
-	int B = 9;
-	
-	for(i = 0; i < 4; i++){
-		for(j = 0; j < 4; j++){
-			ASSERT_EQUAL( B, A ); 
-		}
-	}
-
-}
-
 CTEST(testLife, TestRandomFirstGen) {
 	int A[3][3] = { { 3, 3, 3 },{ 3, 3, 3 },{ 3, 3, 3 } };
 	int B[3][3] = { { 3, 3, 3 },{ 3, 3, 3 },{ 3, 3, 3 } };
 
-	RandomFirstGen(A, 3, 3);
+	random_FirstGen(A, 3, 3);
 	
 	for(i = 0; i < 4; i++){
 		for(j = 0; j < 4; j++){
@@ -59,7 +47,7 @@ CTEST(testLife, TestRename) {
 	int A[3][3] = { { 0, 0, 2 },{ 2, 3, 0 },{ 2, 0, 3 } };
 	int B[3][3] = { { 3, 3, 1 },{ 1, 3, 3 },{ 1, 3, 3 } };
 
-	Rename(A, 3, 3);	
+	rename(A, 3, 3);	
 	
 	for(i = 0; i < 4; i++){
 		for(j = 0; j < 4; j++){
@@ -73,7 +61,7 @@ CTEST(testLife, TestDeathGen) {
 	int A[3][3] = { { 3, 3, 3 },{ 1, 1, 1 },{ 3, 3, 3 } };
 	int B[3][3] = { { 3, 3, 3 },{ 3, 0, 3 },{ 3, 3, 3 } };
 	
-	DeathGen(A, 3, 3);
+	deathGen(A, 3, 3);
 	
 	for(i = 0; i < 4; i++){
 		for(j = 0; j < 4; j++){
@@ -87,7 +75,7 @@ CTEST(testLife, TestSecondGen) {
 	int A[3][3] = { { 3, 3, 3 },{ 1, 3, 1 },{ 3, 1, 3 } };
 	int B[3][3] = { { 3, 3, 3 },{ 3, 2, 3 },{ 3, 3, 3 } };
 	
-	SecondGen(A, 3, 3);
+	secondGen(A, 3, 3);
 	
 	for(i = 0; i < 4; i++){
 		for(j = 0; j < 4; j++){
@@ -101,7 +89,7 @@ CTEST(testLife, TestCustomRename) {
 	int A[3][3] = { { 0, 3, 0 },{ 0, 3, 3 },{ 0, 3, 0 } };
 	int B[3][3] = { { 3, 3, 3 },{ 3, 3, 3 },{ 3, 3, 3 } };
 	
-	CustomRename(A, 3, 3);
+	custom_Rename(A, 3, 3);
 	
 	for(i = 0; i < 4; i++){
 		for(j = 0; j < 4; j++){
@@ -115,7 +103,7 @@ CTEST(testLife, TestCustomSecondGen) {
 	int A[3][3] = { { 3, 3, 3 },{ 6, 3, 3 },{ 3, 3, 3 } };
 	int B[3][3] = { { 3, 3, 3 },{ 6, 2, 3 },{ 3, 3, 3 } };
 	
-	CustomSecondGen(A, 3, 3);
+	custom_SecondGen(A, 3, 3);
 	
 	for(i = 0; i < 4; i++){
 		for(j = 0; j < 4; j++){
@@ -130,7 +118,7 @@ CTEST(testLife, TestCustomDeathGen) {
 	int A[3][3] = { { 3, 3, 3 },{ 3, 6, 3 },{ 3, 3, 3 } };
 	int B[3][3] = { { 3, 3, 3 },{ 3, 0, 3 },{ 3, 3, 3 } };
 	
-	CustomDeathGen(A, 3, 3);
+	custom_DeathGen(A, 3, 3);
 	
 	for(i = 0; i < 4; i++){
 		for(j = 0; j < 4; j++){
